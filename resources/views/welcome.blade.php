299a33ab126e68c28aa1a10eb0d3c65a78018c17
Erick <erickperezquispe@gmail.com>
2018-06-04 16:09:52 -0500

    contenidos en html

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Polleria</title>
+        <!-- Bootstrap -->
+        <link href="{{ asset('css/mdb.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Main Style -->
+    
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!--Fonts-->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" media="screen" href="assets/fonts/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" media="screen" href="assets/fonts/simple-line-icons.css">    
+    <!-- Color CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="assets/css/colors/mblue.css" media="screen" />  
+    <link rel="stylesheet" type="text/css" href="css/general.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/efectoGeneral.css" media="screen" />    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous"> 
+    
+
+
+
 </head>
 <body>
     <h3>Holas</h3>
+    <div class="container red">
+        <p class=" ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque recusandae ut, sapiente nemo ipsa tenetur, nobis repellat rem accusamus totam libero! Voluptatibus laudantium optio incidunt, cumque reiciendis eum asperiores veritatis?
+        </p>
+    </div>
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/mdb.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/general01.js"></script>
+    
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level 
+
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    plugin JavaScript-->
+
+ 
+    <script src="anima/js/modernizr.custom.js"></script>
+
+
+
+
 </html>
